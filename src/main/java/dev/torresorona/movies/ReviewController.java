7856fa6de1dc6a45ace0542835bb31da1257571e
Carlos Torres Orona <ce.torresorona@gmail.com>
2023-09-22 17:15:27 -0400

    changed CrossOrigin annotation to only allow movie client

--- src/main/java/dev/torresorona/movies/ReviewController.java
+++ src/main/java/dev/torresorona/movies/ReviewController.java
@@ -3,13 +3,11 @@ package dev.torresorona.movies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+@CrossOrigin(origins = "https://movie-node-client-7cad6e26a93d.herokuapp.com/")
 @RestController
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
